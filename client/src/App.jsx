9917f4462331b3b2bc122ee519cdffc8c4240ef4
M V Kiran <mada.kiran@s.amity.edu>
2026-06-30 21:25:50 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,1 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import CreateRoom from './pages/CreateRoom'
-import JoinRoom from './pages/JoinRoom'
-import Room from './pages/Room'
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateRoom />} />
-        <Route path="/join" element={<JoinRoom />} />
-        <Route path="/room/:code" element={<Room />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
